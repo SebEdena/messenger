04e55a0538813ed54903922541c928ad0ff5a261
Sébastien Viguier <viguier.sebastien@orange.fr>
2023-05-20 22:18:10 +0200

    barrels

---
+++ common/src/dtos/index.ts
@@ -0,0 +1,10 @@
+export * from "./create-message.dto";
+export * from "./create-room.dto";
+export * from "./create-user.dto";
+export * from "./credentials.dto";
+export * from "./login.dto";
+export * from "./refresh-token.dto";
+export * from "./update-message.dto";
+export * from "./update-room.dto";
+export * from "./update-user.dto";
+
---
+++ common/src/entities/index.ts
@@ -0,0 +1,6 @@
+export * from "./abstract.entity";
+export * from "./message.entity";
+export * from "./refresh-token.entity";
+export * from "./room.entity";
+export * from "./user.entity";
+
